289cc32cd3e8cd01086bc2343653892cc7ac036d
Kenneth Humphries <kennyboyeleete@gmail.com>
2017-05-10 16:54:29 -0400

    Begun work on subsystems

--- src/org/usfirst/frc/team1155/robot/subsystems/SwerveDriveSubsystem.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/SwerveDriveSubsystem.java
@@ -1,5 +1,51 @@
 package org.usfirst.frc.team1155.robot.subsystems;
 
-public class SwerveDriveSubsystem {
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class SwerveDriveSubsystem extends Subsystem{
+	
+	private int FWD, STR, RCW;
+	// TODO: Update for robot's dimensions.
+	private static final double LENGTH = 0;
+	private static final double WIDTH = 0;
+	private static final double DIAGONAL = Math.sqrt(LENGTH*LENGTH + WIDTH*WIDTH);
+	private static double P, I, D;
+	private WheelController frTalon, flTalon, blTalon, brTalon;
+	
+	public SwerveDriveSubsystem() {
+		// TODO: Find corresponding numbers for each talon.
+		frTalon = new WheelController(P, I, D, 0);
+		flTalon = new WheelController(P, I, D, 1);
+		blTalon = new WheelController(P, I, D, 2);
+		brTalon = new WheelController(P, I, D, 3);
+		updateJoystickValues(0,0,0);
+	}
+	protected void initDefaultCommand() {
+		
+	}
+	public int getFWD() {
+		return FWD;
+	}
+	public int getSTR() {
+		return STR;
+	}
+	public int getRCW() {
+		return RCW;
+	}
+
+	public void updateJoystickValues(int f, int s, int r) {
+		FWD = f;
+		STR = s;
+		RCW = r;
+	}
+	
+	public void setPID(double pp, double ii, double dd) {
+		P = pp;
+		I = ii;
+		D = dd;
+	}
+	
 
 }
---
+++ src/org/usfirst/frc/team1155/robot/subsystems/WheelController.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team1155.robot.subsystems;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+public class WheelController extends PIDSubsystem{
+
+	private CANTalon talon;
+	private int desiredAngle;
+	
+	public WheelController(double p, double i, double d, int talonNum) {
+		super(p, i, d);
+		talon = new CANTalon(talonNum);
+		
+	}
+
+	
+	//CODE FOR ADJUSTING BASED ON ENCODER TO BE IMPLEMENTED
+	@Override
+	protected double returnPIDInput() {
+		return 0;
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		
+	}
+	
+	public CANTalon getTalon() {
+		return talon;
+	}
+	
+	public double getSpeed() {
+		return talon.get();
+	}
+	
+	
+	public void setSpeed(double s) {
+		talon.set(s);
+	}
+
+	public void setDesiredAngle(int theta) {
+		desiredAngle = theta;
+	}
+}
