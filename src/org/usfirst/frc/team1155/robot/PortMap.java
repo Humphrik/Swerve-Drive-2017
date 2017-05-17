c618061f6d7c8a6fa174ad4c72f1620f476cd2ae
Angela Luo <Angela Luo@100.104.16.227>
2017-05-17 16:24:30 -0400

    added portmap

---
+++ src/org/usfirst/frc/team1155/robot/PortMap.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team1155.robot;
+
+public class PortMap {
+		
+	//****************JOYSTICKS*******************//
+
+	public static final int JOYSTICK_LEFT = 0;
+	public static final int JOYSTICK_RIGHT = 1;
+	
+	//****************DRIVE*******************//
+	
+	public static final int SPEED_FRONT_LEFT_TALON = 7;
+	public static final int SPEED_FRONT_RIGHT_TALON = 5;
+	public static final int SPEED_BACK_RIGHT_TALON = 2;
+	public static final int SPEED_BACK_LEFT_TALON = 6;
+	public static final int ROTATION_FRONT_LEFT_TALON = 7;
+	public static final int ROTATION_FRONT_RIGHT_TALON = 5;
+	public static final int ROTATION_BACK_RIGHT_TALON = 2;
+	public static final int ROTATION_BACK_LEFT_TALON = 6;
+	
+}
+
+
