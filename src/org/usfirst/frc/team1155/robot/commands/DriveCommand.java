2a0236463c3693c1fc57090c26459449e2438d98
HugGa <galloh@bxscience.edu>
2017-05-22 15:45:30 -0400

    Added a drive command, needs fleshing out tho

---
+++ src/org/usfirst/frc/team1155/robot/commands/DriveCommand.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team1155.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveCommand extends Command {
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
